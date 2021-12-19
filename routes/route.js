7a3a075bdc713205b3847adab6304e442296b530
kdot <alvarezderrick0@gmail.com>
2021-12-18 21:44:18 -0800

    swapped concatenation with template literal

--- routes/route.js
+++ routes/route.js
@@ -20,7 +20,7 @@ module.exports = app => {
             let newNote = req.body;
             notes.push(newNote);
             updateDb();
-            return console.log("Added new note: "+newNote.title);
+            return console.log(`Added new note: ${newNote.title}`);
         });
 
         
